4a484a89431f5d2fa0779b18e9dc2bc4cdd6ba2f
Nauman Bashir <nauman.bashir@hotmail.com>
2025-07-05 21:33:17 +0500

    Added todos to localstorage

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -1,15 +1,17 @@
 import TodoCard from "./components/TodoCard.jsx";
 import TodoInput from "./components/TodoInput.jsx";
 import TodoList from "./components/TodoList.jsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
 
-    const [todos, setTodos] = useState([
-        'Go to Gym',
-        'Do my breakfast',
-        'Study some tutorials',
-    ]);
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        let localTodos = localStorage.getItem("todos");
+        if (!localTodos) return;
+        setTodos(JSON.parse(localTodos).todos);
+    }, [])
 
     const [todoValue, setTodoValue] = useState("");
     const [editingIndex, setEditingIndex] = useState(null);
@@ -17,11 +19,14 @@ function App() {
     const handleAddTodos = (newTodo) => {
         let newTodos = [...todos, newTodo];
         setTodos(newTodos);
+        setTodoValue("");
+        persistData(newTodos)
     }
 
     const handleDeleteTodo = (index) => {
         const newTodos = todos.filter((todo, todoIndex) => todoIndex !== index);
         setTodos(newTodos);
+        // persistData(newTodos);
     }
 
     const handleEditTodo = (index) => {
@@ -32,13 +37,21 @@ function App() {
 
     const handleUpdateTodo = (updatedTodo) => {
         if (editingIndex === null || !updatedTodo.trim()) return;
-        setTodos((prev) => {
-            return prev.map((todo, todoIndex) => {
-                return (editingIndex === todoIndex) ? updatedTodo : todo;
-            })
+        const newTodos = todos.map((todo, todoIndex) => {
+            return (editingIndex === todoIndex) ? updatedTodo : todo;
         })
+
+        setTodos(newTodos);
+        persistData(newTodos);
+
         setEditingIndex(null);
         setTodoValue("");
+    }
+
+    function persistData(newTodos) {
+        localStorage.setItem('todos', JSON.stringify({
+            todos: newTodos
+        }));
     }
 
     return (
